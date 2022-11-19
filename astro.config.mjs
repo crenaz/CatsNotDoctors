695775e7718e4ba56ed838c7f6616aa2e308494e
crenaz <crenaz@gmail.com>
2022-11-19 12:15:39 -0400

    1215

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,16 +12,15 @@ import image from "@astrojs/image";
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+// https://astro.build/config THIS IS NOT NEEDED FOR VERCEL import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), alpinejs(), image(), mdx(), sitemap()],
+  site: "https://cats-not-doctors.vercel.app/",
+  integrations: [tailwind(), alpinejs(), image(), mdx()],
   output: "server",
   adapter: vercel()
 });
