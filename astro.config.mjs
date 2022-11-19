e26dce7831e592a076fa445746c8fcfd3792dc3c
crenaz <crenaz@gmail.com>
2022-11-19 11:58:42 -0400

    1158

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,7 +16,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
-  integrations: [tailwind(), alpinejs(), image(), mdx(), sitemap()]
+  integrations: [tailwind(), alpinejs(), image(), mdx(), sitemap()],
+  output: "server",
+  adapter: vercel()
 });
